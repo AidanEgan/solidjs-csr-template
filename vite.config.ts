278e57b2bbcc3d9fc96c4993c69dc2d25ea2643c
“AidanEgan” <aidanegan19@gmail.com>
2025-08-04 22:40:57 -0400

    Help wrangle bundle sizes

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,17 @@ import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import { analyzer } from "vite-bundle-analyzer";
 import solidPlugin from "vite-plugin-solid";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+const optionalAnalyzer = process.env?.VITE_ANALYZE
+	? [analyzer({ analyzerPort: 3002 })]
+	: [];
 
 export default defineConfig({
-	plugins: [solidPlugin(), tailwindcss()],
+	plugins: [solidPlugin(), tailwindcss(), ...optionalAnalyzer],
 	server: {
 		port: 3001,
 		open: true,
