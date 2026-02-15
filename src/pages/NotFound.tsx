c16d6493008c3a34fd6e49f22e33fc41a42bba9d
“AidanEgan” <8503684+AidanEgan@users.noreply.github.com>
2026-02-15 15:23:59 -0500

    Update deps

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -4,7 +4,7 @@ import { useRouter } from "@/hooks/useRouter";
 export const NotFound = () => {
 	const navigate = useRouter();
 	return (
-		<div class="m-10 p-4 pt-10 border-blue-950 border-4 flex flex-col items-center min-h-[200px]">
+		<div class="m-10 p-4 pt-10 border-blue-950 border-4 flex flex-col items-center min-h-50">
 			<h1> Oh no! That route doesn't exist! </h1>
 			<Button class="mx-auto mt-5" onClick={() => navigate("/")}>
 				{" "}
